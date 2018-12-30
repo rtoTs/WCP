847f2e47b3329a45fc6d15c231b94b6797f8d23d
Junya Koseki <junya_koseki@JunyanoMacBook.local>
2018-12-31 02:29:40 +0900

    [END] Railsを学ぼう

---
+++ 99_Railsを学ぼう/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e7f9248da480bafae93e1191b85476fe0b46d7ce5432cd3057f3919b1b0737ec604ddf3125d3274b88a3f02580037bc5a8a97aaa76475430a55cc1744b721a24'
