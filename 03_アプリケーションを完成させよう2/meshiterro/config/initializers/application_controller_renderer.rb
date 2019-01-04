db0c837a6a97cc7042bba3330ee91abf93a73be6
Junya Koseki <junya_koseki@JunyanoMacBook.local>
2019-01-04 15:22:10 +0900

    END: アプリケーションを完成させよう2 01〜08章

---
+++ 03_アプリケーションを完成させよう2/meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a05311344b7fc05ddbdbf6b9dfad32aa094dac0d3241e49965292cc889beeb7c08efab9cd8f0a84791a0721166b6cc47b1d86e2980f7609c855155fc3738c10d'
